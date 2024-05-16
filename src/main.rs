ce9d7cde4303964fe764532aa8d10b4297a57141
kjura <30084359+kjura@users.noreply.github.com>
2024-05-16 14:37:33 +0200

    feat: add possibility to replace values from str to int necessary for Perceptron

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use polars::prelude::*;
+#![allow(dead_code)]
+#![allow(unused)]
+use polars::{lazy::dsl::lit, prelude::*};
 
 fn print_whole_frame(s: &Series) {
-    println!("{}", &s)
+    todo!()
 }
 
 // We have four features for iris, so 4 weights
 fn initalize_weights(weights: Series) -> Series{
-    weights
+    todo!()
 }
 
 fn initalize_bias(){
@@ -28,16 +30,11 @@ fn update_bias(){
 
 fn dot_produt(x_left: Series, x_right: Series) -> PolarsResult<f64> {
     
-    (x_left * x_right).sum() 
+    todo!() 
 }
 
 fn sigma(z: f64) -> u8 {
-    if z >= 0.0 {
-        1
-    }
-    else {
-        0
-    }
+    todo!()
 }
 
 
@@ -49,27 +46,55 @@ fn main() {
     .finish()
     .unwrap();
 
-
     // "sepal.length","sepal.width","petal.length","petal.width","variety"
-    // let col_variety: DataFrame = iris_dataset.clone().lazy().select([col("variety")]).collect().unwrap();
-    let variety = &iris_dataset["variety"];
-    let sepal_length = &iris_dataset["sepal.length"];
-    let sepal_width = &iris_dataset["sepal.width"];
-    let petal_length = &iris_dataset["petal.length"];
-    let petal_width = &iris_dataset["petal.width"];
-    
-    // print_whole_frame(variety);
-    // print_whole_frame(sepal_length);
-    // print_whole_frame(sepal_width);
-    // print_whole_frame(petal_length);
-    // print_whole_frame(petal_width);
+    // Take sepal.length and petal.length only
+    // restrict to setosa and versicolor
 
-    let s = Series::new("a", [1 , 2, 3]);
-    let s_two = Series::new("b", [2 , 4, 6]);
-    let dot = dot_produt(s, s_two).unwrap();
-    println!("{}", dot);
-    // let out_mul = &s * &s;
-    // println!("{}", out_mul);
-    // println!("Finished!");
+    let feature_matrix = iris_dataset
+        .clone()
+        .lazy()
+        .select([cols(["sepal.length", "petal.length"])])
+        .collect()
+        .unwrap();
+
+    let mut x_train = feature_matrix.clone().slice(0, 100);
+    let mut x_test = feature_matrix.clone().slice(100, iris_dataset.shape().0);
+
+    let mut x_train_sepal_petal_length = x_train.clone().select(["sepal.length", "petal.length"]);
+    let mut y_train = 
+        iris_dataset
+        .clone()
+        .lazy()
+        .slice(0, 100)
+        .with_column(
+            when(col("variety").eq(lit("Versicolor"))).then(lit("kubus")).otherwise(lit("nic")).alias("haha")
+        )
+        .collect()
+        .unwrap();
+    
+    let check_encoding_series = 
+    y_train
+    .clone()
+    .select(["haha"])
+    .unwrap()
+    .select_series(["haha"])
+    .unwrap();
+
+    println!("{}", y_train);
+    println!("{}", check_encoding_series[0].unique().unwrap());
+
+    // let mut weights = vec![0.12, 0.34];
+
+    // let eta: f64 = 0.01;
+    // let number_of_iterations: u8 = 80;
+    // let seed: u8 = 42;
+    // let bias = 0.0;
+    
+    // .select([col("variety")])
+    // .collect()
+    // .unwrap();
+
+    // let weights = Series::new("weights", [0.0, 0.0, 0.0, 0.0]);
+
 
 }
